93a5ae207465b85b453f2fbe2883b431484c7f4b
Thaddeus Cleo <thaddeuscleo@gmail.com>
2021-08-16 15:08:39 +0700

    [Add] custom controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $remember_me = $request->post('remember_me') ? true : false;
+
+        if (Auth::attempt($credentials, $remember_me)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('home');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+}
